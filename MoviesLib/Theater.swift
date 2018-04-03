b730a670f4060263e70e5435b4cc327e4a78e6a1
Usuário Convidado <Guest@pa06micro15.local>
2018-04-02 21:37:27 -0300

    segunda parte

---
+++ MoviesLib/Theater.swift
@@ -0,0 +1,12 @@
+
+import Foundation
+
+struct Theater {
+    var name: String = ""
+    var address: String = ""
+    var latitude: Double = 0.0
+    var longitude: Double = 0.0
+    var url: String = ""
+}
+
+
